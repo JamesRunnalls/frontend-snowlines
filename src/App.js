91120c1de3dc32ed025be9d60adcfe6cb7ab0d17
James Runnalls <james.runnalls@eawag.ch>
2021-01-04 17:03:14 +0100

    Add analytics

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/home";
 import NotFound from "./pages/notfound/notfound";
+import ReactGA from "react-ga";
+ReactGA.initialize("G-8EPEJKZPC5");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
   render() {
